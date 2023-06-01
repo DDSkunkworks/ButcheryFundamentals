acb778e8716000e9b7cc824b4cb03d68d8120be2
Mas Tsukada <46246538+TsukadaMas@users.noreply.github.com>
2023-06-01 16:21:54 -0400

    update yaml

--- next.config.js
+++ next.config.js
@@ -1,17 +1,13 @@
-const withTM = require('next-transpile-modules')(['three'])
-
-
 /**
 * @type {import('next').NextConfig}
 */
 const nextConfig = {
+  transpilePackages: ['three'],
     output: 'export',
     // Optional: Add a trailing slash to all paths `/about` -> `/about/`
     // trailingSlash: true,
     // Optional: Change the output directory `out` -> `dist`
     //distDir: 'dist',
   };
-
-module.exports = withTM()
 
 module.exports = nextConfig;
